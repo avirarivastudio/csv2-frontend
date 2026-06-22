394bfc46b2672622a98ec6c39c4a357fd046885a
avira.studio <avira.studio@gmail.com>
2026-06-22 10:59:45 +0700

    .

--- mygit.bat
+++ mygit.bat
@@ -1,11 +1,4 @@
-# 1. Cek file apa saja yang berubah (opsional, untuk memastikan)
 git status
-
-# 2. Tambahkan semua perubahan file ke staging area
 git add .
-
-# 3. Berikan catatan/pesan tentang perubahan tersebut
-git commit -m "Deskripsi perubahan yang kamu lakukan"
-
-# 4. Push langsung ke GitHub
+git commit -m "."
 git push origin main
